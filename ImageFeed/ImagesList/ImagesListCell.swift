9c63c7872bf0599b1dad26d6da7b2049500e0305
Marsel Abdrakhmanov <mnabdrakhmanov@avito.ru>
2025-02-16 20:11:49 +0300

    реализация требований 8 спринта

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by mnabdrakhmanov on 09.02.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
