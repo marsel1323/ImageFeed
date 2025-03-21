4f58f943b230fbb88fa7965532afa7b991561460
Marsel Abdrakhmanov <mnabdrakhmanov@avito.ru>
2025-03-21 14:58:02 +0300

    remove commented code

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,14 +8,6 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    //    @IBOutlet private var avatarImageView: UIImageView!
-    //    @IBOutlet private var nameLabel: UILabel!
-    //    @IBOutlet private var loginNameLabel: UILabel!
-    //    @IBOutlet private var descriptionLabel: UILabel!
-    
-    //    @IBOutlet private var logoutButton: UIButton!
-    
-//    @IBAction private func didTapLogoutButton(_ sender: Any) {}
     
     // MARK: - UI Elements
     
